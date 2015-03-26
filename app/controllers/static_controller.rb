917510b9b947fd05252aa1bb3ef6fa3792595735
cyncastic <cyncastic@gmail.com>
2015-03-26 18:43:26 -0400

    initial static pages

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,19 @@
+class StaticController < ApplicationController
+  def home
+  end
+
+  def artwork
+  end
+
+  def instagram
+  end
+
+  def photos
+  end
+
+  def abstract
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'static/home'
+
+  get 'static/artwork'
+
+  get 'static/instagram'
+
+  get 'static/photos'
+
+  get 'static/abstract'
+
+  get 'static/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
