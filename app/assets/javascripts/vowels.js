a1cc3a2b0cc41dd7d9496957fe740f5447af8a2d
cyncastic <cyncastic@gmail.com>
2015-03-29 13:40:24 -0400

    vowels done

--- app/assets/javascripts/vowels.js
+++ app/assets/javascripts/vowels.js
@@ -1,8 +1,25 @@
 $(document).ready(function($) {
-  $(".nav-link a").each( function() {
-    $( this ).html(
-      $( this ).html().replace(/(a|e|i|o|u)/ig, "<span class='vowel'>$1</span>")
 
+  // add a class of 'vowel' to each vowel using regex
+  $(".nav-link a").each( function() {
+    $(this).html(
+      $(this).html().replace(/(a|e|i|o|u)/ig, "<span class='vowel'>$1</span>")
     );
   });
+
+  $(".vowel").each( function() {    
+    var myVowel = $(this);
+    myVowel.data('width', myVowel.width());
+    myVowel.width(0);
+
+    myVowel.parent().hover( 
+      function(){
+        myVowel.width(myVowel.data('width'));
+      }, function(){
+        myVowel.width(0);
+      }
+    );
+    
+  });
+  
 });
