e7ee1ba9e09f696937581cb61fca87068e0e3f35
cyncastic <cyncastic@gmail.com>
2015-04-14 13:42:01 -0400

    restaging for artwork section

---
+++ app/assets/javascripts/artwork.js
@@ -0,0 +1,12 @@
+$(document).ready(function($) {
+
+  var $mainImg = $('.artwork-holder > img');
+
+  $('.artwork-conveyor a').click( function(){
+    event.preventDefault();
+    $mainImg.attr('src', this.href);
+    console.log(this.href);
+  });
+
+
+});
