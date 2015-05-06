99d61eb41712ca890b525edc1217585736bef185
cyncastic <cyncastic@gmail.com>
2015-05-05 23:10:29 -0400

    fix controller and add sectionadmin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,41 @@
 class UsersController < ApplicationController
-  # GET /users
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
 
-  # GET /users/1
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
-
+    
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      redirect_to users_url, notice: "User #{@user.name} was successfully created."
     else
-      render action: 'new'
+      render action: "new"
     end
   end
 
-  # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+    if @user.update_attributes(user_params)
+      redirect_to users_url, notice: "User #{@user.name} was successfully updated."
     else
-      render action: 'edit'
+      render action: "edit"
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    redirect_to users_url, notice: 'User deleted'
   end
 
   private
