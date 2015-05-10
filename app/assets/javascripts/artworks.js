9dcbea689e925d2adc3886073345504bf0573758
cyncastic <cyncastic@gmail.com>
2015-05-10 01:46:48 -0400

    infinite scroll beta

--- app/assets/javascripts/artworks.js
+++ app/assets/javascripts/artworks.js
@@ -2,8 +2,7 @@ var setCarousel = function () {
 
   $('.artwork-conveyor').each(function () {
 
-    var $prev = $('.prev', this),
-      $next = $('.next', this),
+    var $toggle = $('.toggle', this),
       $carousel = $('ul', this),
       $seats = $('.artwork-link', this);
 
@@ -17,26 +16,46 @@ var setCarousel = function () {
       return e;
     };
 
+    var prev = function (el) {
+      var e;
+      if (el.prev().length > 0) {
+        e = el.prev();
+      } else {
+        e = $seats.last();
+      }
+      return e;
+    };
 
-    $next.click(function () {
+
+    $toggle.click(function () {
       var el, i, j, new_seat, ref;
 
-      $carousel.removeClass('is-set');
-
       el = $('.is-ref', $carousel).removeClass('is-ref');
-      new_seat = next(el);
+
+      if ($(this).hasClass('next')) {
+        new_seat = next(el);
+        $carousel.removeClass('is-reversing');
+      } else {
+        new_seat = prev(el);
+        $carousel.addClass('is-reversing');
+      }
+
+
+
+
       $(new_seat).addClass('is-ref').css('order', 1);
 
       for (i = j = 2, ref = $seats.length; 2 <= ref ? j <= ref : j >= ref; i = 2 <= ref ? ++j : --j) {
         new_seat = next(new_seat).css('order', i);
       }
 
+      $carousel.removeClass('is-set');
+      setTimeout(function () {
+        return $carousel.addClass('is-set');
+      }, 50);
+
     });
 
-
-    $prev.click(function () {
-      return false;
-    });
 
 
   });
