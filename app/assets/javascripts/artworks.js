e67d27c707855294a2cea8c1163a2c7107d81754
cyncastic <cyncastic@gmail.com>
2015-05-09 23:46:09 -0400

    first try for infinite flex

--- app/assets/javascripts/artworks.js
+++ app/assets/javascripts/artworks.js
@@ -1,47 +1,61 @@
 var setCarousel = function () {
 
+  $('.artwork-conveyor').each(function () {
+
+    var $prev = $('.prev', this),
+      $next = $('.next', this),
+      $carousel = $('ul', this),
+      $seats = $('.artwork-link', this);
+
+    var next = function (el) {
+      var e;
+      if (el.next().length > 0) {
+        e = el.next();
+      } else {
+        e = $seats.first();
+      }
+      return e;
+    };
+
+
+    $next.click(function () {
+      var el, i, j, new_seat, ref;
+
+      $carousel.removeClass('is-set');
+
+      el = $('.is-ref', $carousel).removeClass('is-ref');
+      new_seat = next(el);
+      $(new_seat).addClass('is-ref').css('order', 1);
+
+      for (i = j = 2, ref = $seats.length; 2 <= ref ? j <= ref : j >= ref; i = 2 <= ref ? ++j : --j) {
+        new_seat = next(new_seat).css('order', i);
+      }
+
+    });
+
+
+    $prev.click(function () {
+      return false;
+    });
+
+
+  });
+
+
+
+
+
+
+
+
   var $artworks = $('.artwork-link a');
 
   $('img', $artworks).load(function () {
     $(this).parent('a').removeClass('loading');
   });
 
-  $artworks.click(function (e) {
-    e.preventDefault();
-  });
+  $artworks.click(function (e) { e.preventDefault(); });
 
-  //Conveyor belt functions
-  $('.artwork-conveyor').each(function () {
-
-    var $prev = $('.prev', this),
-      $next = $('.next', this),
-      $carousel = $('ul', this),
-      totalWidth = $carousel.prop('scrollWidth'),
-      increment =  $carousel.width() + 24;
-
-    if (totalWidth > increment) { $next.addClass('active'); }
-
-    $prev.click(function () {
-      $next.addClass('active');
-      if (parseInt($carousel.css('marginLeft'), 10) + increment <= 0) {
-        $carousel.css('marginLeft', '+=' + increment);
-      } else {
-        $carousel.css('marginLeft', 0);
-        $prev.removeClass('active');
-      }
-    });
-
-    $next.click(function () {
-      $prev.addClass('active');
-      if (parseInt($carousel.css('marginLeft'), 10) - increment > -(totalWidth - increment)) {
-        $carousel.css('marginLeft', '-=' + increment);
-      } else {
-        $carousel.css('marginLeft', -(totalWidth - increment + 24));
-        $next.removeClass('active');
-      }
-    });
-
-  });
 
 };
 
